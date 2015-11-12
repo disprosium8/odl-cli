328fc37543ef4fe21d8a31d6e359d8024b5a5b3d
Ezra Kissel <ezkissel@indiana.edu>
2015-11-12 17:51:24 -0600

    Adding push_flow script. Updated test_flow.js

--- json/test_flow.js
+++ json/test_flow.js
@@ -1,24 +1,24 @@
 {
-    'flow': {
-	'cookie': 0,
-	'flags': '',
-	'id': '298a',
-	'instructions': {
-	    'instruction': [{
-		'apply-actions': {
-		    'action': [{
-			'order': 1,
-			'push-vlan-action': {'ethernet-type': 33024}},
-			       {'order': 2,
-				'set-field': {'vlan-match': {'vlan-id': {'vlan-id': 298,
-									 'vlan-id-present': True}}}},
-			       {'order': 0,
-				'output-action': {'max-length': 0,
-						  'output-node-connector': '37'}}]},
-		'order': 0}]},
-	'match': {'ethernet-match': {'ethernet-type': {'type': 33024}},
-		  'vlan-match': {'vlan-id': {'vlan-id': 298,
-					     'vlan-id-present': True}}},
-	'priority': 500,
-	'table_id': 0}
+    "flow": {
+	"cookie": 0,
+	"flags": "",
+	"id": "298a",
+	"instructions": {
+	    "instruction": [{
+		"apply-actions": {
+		    "action": [{
+			"order": 1,
+			"push-vlan-action": {"ethernet-type": 33024}},
+			       {"order": 2,
+				"set-field": {"vlan-match": {"vlan-id": {"vlan-id": 298,
+									 "vlan-id-present": true}}}},
+			       {"order": 0,
+				"output-action": {"max-length": 0,
+						  "output-node-connector": "37"}}]},
+		"order": 0}]},
+	"match": {"ethernet-match": {"ethernet-type": {"type": 33024}},
+		  "vlan-match": {"vlan-id": {"vlan-id": 298,
+					     "vlan-id-present": true}}},
+	"priority": 500,
+	"table_id": 0}
 }
---
+++ push_flows.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+'''
+Usage:
+push_flows [<flow_file> <url> <user> <password>]
+'''
+import sys
+sys.path.append('python-odl-0.0.1')
+from odl.instance import ODLInstance
+from odl.topology import ODLTopology
+from odl.node import ODLNode
+from docopt import docopt
+import json
+
+if __name__ == '__main__':
+    args = docopt(__doc__, version='odl-client 0.1')
+    url = args.get("<url>")
+    if not url:
+        url = "http://odl.scinet.ncsa.edu:8181"
+        
+    user = args.get("<user>")
+    if not user:
+        user = "admin"
+
+    pw = args.get("<password>")
+    if not pw:
+        pw = "admin"
+
+    ffile = args.get("<flow_file>")
+        
+    info =\
+"""File  : %s
+Server: %s
+User  : %s
+Passwd: %s\n""" % (ffile, url, user, "*****" if pw != "admin" else pw)
+    print info
+
+    try:
+        f = open(ffile, 'r')
+        fstr = f.read()
+        flows = json.loads(fstr)
+    except Exception, e:
+        print "Error: %s" % e
+
+    odl = ODLInstance(url, (user, pw))
+    for flow in flows:
+        try:
+            print "Pushing flow entry for connection %s to switch %s" % \
+                (flow['id'], flow['switch'])
+            #self.odl.put_flow(flow, self.node.id, table, fid)
+        except Exception, e:
+            print "Error pushing flow: %s" % e
+
+    f.close()
+    
